cde89410cb3ecd5f73d6047d4b1ae2e22b360649
husty530 <husty530@gmail.com>
2020-10-07 00:15:59 +0900

    *

---
+++ LabelingTool/View.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LabelingTool
+{
+    partial class View
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenDirButton = new System.Windows.Forms.Button();
+            this.SaveDirButton = new System.Windows.Forms.Button();
+            this.UndoButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.WidthTx = new System.Windows.Forms.TextBox();
+            this.ExtensionTx = new System.Windows.Forms.TextBox();
+            this.HeightTx = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenDirButton
+            // 
+            this.OpenDirButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OpenDirButton.Location = new System.Drawing.Point(12, 12);
+            this.OpenDirButton.Name = "OpenDirButton";
+            this.OpenDirButton.Size = new System.Drawing.Size(114, 47);
+            this.OpenDirButton.TabIndex = 0;
+            this.OpenDirButton.Text = "Open Dir";
+            this.OpenDirButton.UseVisualStyleBackColor = true;
+            this.OpenDirButton.Click += new System.EventHandler(this.OpenDirButton_Click);
+            // 
+            // SaveDirButton
+            // 
+            this.SaveDirButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaveDirButton.Location = new System.Drawing.Point(132, 12);
+            this.SaveDirButton.Name = "SaveDirButton";
+            this.SaveDirButton.Size = new System.Drawing.Size(114, 47);
+            this.SaveDirButton.TabIndex = 1;
+            this.SaveDirButton.Text = "Select Save Dir";
+            this.SaveDirButton.UseVisualStyleBackColor = true;
+            this.SaveDirButton.Click += new System.EventHandler(this.SaveDirButton_Click);
+            // 
+            // UndoButton
+            // 
+            this.UndoButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.UndoButton.Location = new System.Drawing.Point(638, 12);
+            this.UndoButton.Name = "UndoButton";
+            this.UndoButton.Size = new System.Drawing.Size(114, 47);
+            this.UndoButton.TabIndex = 2;
+            this.UndoButton.Text = "Undo";
+            this.UndoButton.UseVisualStyleBackColor = true;
+            this.UndoButton.Click += new System.EventHandler(this.UndoButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ClearButton.Location = new System.Drawing.Point(758, 12);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(114, 47);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Yu Gothic UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SaveButton.Location = new System.Drawing.Point(518, 12);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(114, 47);
+            this.SaveButton.TabIndex = 4;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BackButton.ForeColor = System.Drawing.Color.Maroon;
+            this.BackButton.Location = new System.Drawing.Point(406, 12);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(50, 47);
+            this.BackButton.TabIndex = 5;
+            this.BackButton.Text = "◀";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NextButton.ForeColor = System.Drawing.Color.Navy;
+            this.NextButton.Location = new System.Drawing.Point(462, 12);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(50, 47);
+            this.NextButton.TabIndex = 6;
+            this.NextButton.Text = "▶";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(13, 66);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(252, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Extension";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(252, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Size";
+            // 
+            // WidthTx
+            // 
+            this.WidthTx.Location = new System.Drawing.Point(316, 36);
+            this.WidthTx.Name = "WidthTx";
+            this.WidthTx.Size = new System.Drawing.Size(39, 23);
+            this.WidthTx.TabIndex = 10;
+            this.WidthTx.Text = "640";
+            // 
+            // ExtensionTx
+            // 
+            this.ExtensionTx.Location = new System.Drawing.Point(316, 12);
+            this.ExtensionTx.Name = "ExtensionTx";
+            this.ExtensionTx.Size = new System.Drawing.Size(83, 23);
+            this.ExtensionTx.TabIndex = 11;
+            this.ExtensionTx.Text = ".png";
+            // 
+            // HeightTx
+            // 
+            this.HeightTx.Location = new System.Drawing.Point(360, 36);
+            this.HeightTx.Name = "HeightTx";
+            this.HeightTx.Size = new System.Drawing.Size(39, 23);
+            this.HeightTx.TabIndex = 12;
+            this.HeightTx.Text = "480";
+            // 
+            // View
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 711);
+            this.Controls.Add(this.HeightTx);
+            this.Controls.Add(this.ExtensionTx);
+            this.Controls.Add(this.WidthTx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.UndoButton);
+            this.Controls.Add(this.SaveDirButton);
+            this.Controls.Add(this.OpenDirButton);
+            this.Name = "View";
+            this.Text = "Labeling Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenDirButton;
+        private System.Windows.Forms.Button SaveDirButton;
+        private System.Windows.Forms.Button UndoButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox WidthTx;
+        private System.Windows.Forms.TextBox ExtensionTx;
+        private System.Windows.Forms.TextBox HeightTx;
+    }
+}
+
---
+++ LabelingTool/View.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenCvSharp.Extensions;
+
+namespace LabelingTool
+{
+    public partial class View : Form
+    {
+        private bool _mouseMove;
+
+        public View()
+        {
+            InitializeComponent();
+            SaveDirButton.Enabled = false;
+            BackButton.Enabled = false;
+            NextButton.Enabled = false;
+            SaveButton.Enabled = false;
+            UndoButton.Enabled = false;
+            ClearButton.Enabled = false;
+            _mouseMove = false;
+        }
+
+        private void OpenDirButton_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.Description = "画像フォルダを選択";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                var img = Processing.Initialize(fbd.SelectedPath, ExtensionTx.Text, int.Parse(WidthTx.Text), int.Parse(HeightTx.Text));
+                pictureBox.Width = img.Width;
+                pictureBox.Height = img.Height;
+                pictureBox.Image = img.ToBitmap();
+                SaveDirButton.Enabled = true;
+                BackButton.Enabled = false;
+                NextButton.Enabled = false;
+                SaveButton.Enabled = false;
+                UndoButton.Enabled = false;
+                ClearButton.Enabled = false;
+            }
+        }
+
+        private void SaveDirButton_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            fbd.Description = "保存先を選択";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Processing.SaveDirectory = fbd.SelectedPath;
+                pictureBox.Enabled = true;
+                if (Processing.FrameNumber != Processing.FileCount - 1) NextButton.Enabled = true;
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            var img = Processing.GoBack();
+            pictureBox.Image = img.ToBitmap();
+            NextButton.Enabled = true;
+            if (Processing.FrameNumber == 0) BackButton.Enabled = false;
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            var img = Processing.GoNext();
+            pictureBox.Image = img.ToBitmap();
+            BackButton.Enabled = true;
+            if (Processing.FrameNumber == Processing.FileCount - 1) NextButton.Enabled = false;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Processing.Save();
+            SaveButton.Enabled = false;
+        }
+
+        private void UndoButton_Click(object sender, EventArgs e)
+        {
+            var img = Processing.RemoveLast();
+            pictureBox.Image = img.ToBitmap();
+            SaveButton.Enabled = true;
+            if (Processing.RectCount == 0)
+            {
+                UndoButton.Enabled = false;
+                ClearButton.Enabled = false;
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            var img = Processing.Clear();
+            pictureBox.Image = img.ToBitmap();
+            UndoButton.Enabled = false;
+            ClearButton.Enabled = false;
+            SaveButton.Enabled = false;
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseMove = true;
+            var img = Processing.SelectStart(e.X, e.Y);
+            pictureBox.Image = img.ToBitmap();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mouseMove)
+            {
+                var img = Processing.Drag(e.X, e.Y);
+                pictureBox.Image = img.ToBitmap();
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!_mouseMove) return;
+            _mouseMove = false;
+            var img = Processing.SelectGoal(e.X, e.Y);
+            pictureBox.Image = img.ToBitmap();
+            UndoButton.Enabled = true;
+            ClearButton.Enabled = true;
+            SaveButton.Enabled = true;
+        }
+    }
+}
