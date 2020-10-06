e57b1426d25eeb478dfa3d4506582e3a60d4800b
husty530 <husty530@gmail.com>
2020-10-06 21:32:07 +0900

    *

--- CsApp/View.cs
+++ CsApp/View.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 using YoloSharp;
+using System.IO;
 
 namespace CsApp
 {
@@ -33,6 +34,7 @@ namespace CsApp
             var weightsPath = WeightsTx.Text;
             var confThresh = float.Parse(ConfTx.Text);
             var nmsThresh = float.Parse(NmsTx.Text);
+            if (!File.Exists(cfgPath) || !File.Exists(namesPath) || !File.Exists(weightsPath)) return;
             _detector = new Detector();
             _detector.InitializeDetector(cfgPath, namesPath, weightsPath, new Size(384, 288), confThresh, nmsThresh);
             OpenButton.Enabled = true;
@@ -45,6 +47,8 @@ namespace CsApp
             op.Title = "画像ファイルを開く";
             if (op.ShowDialog() == DialogResult.OK)
             {
+                var ext = Path.GetExtension(op.FileName);
+                if (ext != ".jpg" && ext != ".png") return;
                 img = new Mat(op.FileName);
                 Cv2.Resize(img, img, new Size(640, 480));
                 pictureBox.Image = img.ToBitmap();
@@ -59,7 +63,7 @@ namespace CsApp
             var results = _detector.Run(img);
             pictureBox.Image = results.OutputImg.ToBitmap();
             sw.Stop();
-            Time.Text = $"Runtime: {sw.ElapsedMilliseconds} ms";
+            Time.Text = $"Runtime : {sw.ElapsedMilliseconds} ms";
             RunButton.Enabled = false;
         }
     }
